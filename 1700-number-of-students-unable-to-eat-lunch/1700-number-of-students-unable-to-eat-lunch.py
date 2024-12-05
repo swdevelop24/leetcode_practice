2d9ca5e28c924f6a67a50c96a8ed627e804ae36b
swdevelop24 <154391675+swdevelop24@users.noreply.github.com>
2024-12-05 11:20:54 -0600

    Time: 0 ms (100.00%), Space: 17.6 MB (7.12%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,21 +1,15 @@
+from collections import Counter
+
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        # 학생들의 선호도를 카운트
-        cnt = {}
-        for s in students:
-            if s not in cnt:
-                cnt[s] = 0
-            cnt[s] += 1
-
-        # 초기 학생 수
         res = len(students)
-
-        # 샌드위치를 처리
+        cnt = Counter(students)  # 학생 선호도 카운트
+        
         for s in sandwiches:
-            if cnt.get(s, 0)> 0:  # 샌드위치를 원하는 학생이 있는 경우
+            if cnt[s] > 0:  # 현재 샌드위치를 원하는 학생이 있는 경우
                 res -= 1
                 cnt[s] -= 1
-            else:  # 샌드위치를 원하는 학생이 없는 경우
+            else:  # 원하는 학생이 없는 경우 종료
                 return res
-
+        
         return res
